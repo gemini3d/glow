c9233f72c58909912f86a9f9c76f4162957a900c
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2019-10-08 09:56:59 -0400

    glowbasic: correct syntax, use .nml input file
    glowbasic: correct syntax, use .nml input file

--- glowbasic.f90
+++ glowbasic.f90
@@ -31,123 +31,124 @@ program glowbasic
 ! nw      number of airglow emission wavelengths
 ! nc      number of component production terms for each emission
 
-  use cglow,only: jmax,nbins,lmax,nmaj,nei,nex,nw,nc,nst
-  use cglow,only: idate,ut,glat,glong,f107a,f107,f107p,ap,ef,ec
-  use cglow,only: iscale,jlocal,kchem,xuvfac
-  use cglow,only: sza,dip,efrac,ierr
-  use cglow,only: zz,zo,zn2,zo2,zns,znd,zno,ztn,ze,zti,zte
-  use cglow,only: ener,del,phitop,wave1,wave2,sflux,pespec,sespec,uflx,dflx,sion
-  use cglow,only: photoi,photod,phono,aglw,tei,tpi,tir,ecalc,zxden,zeta,zceta,zlbh
-  use cglow,only: cglow_init
-  use cglow,only: data_dir
+use, intrinsic :: iso_fortran_env, only: stdout=>output_unit, stdin=>input_unit
 
-  implicit none
+use cglow,only: jmax,nbins,lmax,nmaj,nei,nex,nw,nc,nst
+use cglow,only: idate,ut,glat,glong,f107a,f107,f107p,ap,ef,ec
+use cglow,only: iscale,jlocal,kchem,xuvfac
+use cglow,only: sza,dip,efrac,ierr
+use cglow,only: zz,zo,zn2,zo2,zns,znd,zno,ztn,ze,zti,zte
+use cglow,only: ener,del,phitop,wave1,wave2,sflux,pespec,sespec,uflx,dflx,sion
+use cglow,only: photoi,photod,phono,aglw,tei,tpi,tir,ecalc,zxden,zeta,zceta,zlbh
+use cglow,only: cglow_init
+use cglow,only: data_dir
 
-  character(len=1024) :: iri90_dir
+implicit none
 
-  real,allocatable :: z(:)                    ! glow height coordinate in km (jmax)
-  real,allocatable :: zun(:), zvn(:)          ! neutral wind components (not in use)
-  real,allocatable :: pedcond(:), hallcond(:) ! Pederson and Hall conductivities in S/m (mho)
-  real,allocatable :: outf(:,:)               ! iri output (11,jmax)
-  real :: rz12,stl,fmono,emono
-  real :: d(8), t(2), sw(25), oarr(30)
-  integer :: l,j,jj,ijf,jmag,iday,mmdd,i,ii,n,k,ix,itail
-  integer :: instance,iostatus
-  logical :: jf(12)
-  data sw/25*1./
+character(:), allocatable :: iri90_dir
+integer :: u
+character(1024) :: config_filename
+
+namelist /input/ idate,ut,glat,glong,f107a,f107,f107p,ap,ef,ec
+
+real,allocatable :: z(:)                    ! glow height coordinate in km (jmax)
+real,allocatable :: zun(:), zvn(:)          ! neutral wind components (not in use)
+real,allocatable :: pedcond(:), hallcond(:) ! Pederson and Hall conductivities in S/m (mho)
+real,allocatable :: outf(:,:)               ! iri output (11,jmax)
+real :: rz12,stl,fmono,emono
+real :: d(8), t(2), sw(25), oarr(30)
+integer :: l,j,jj,ijf,jmag,iday,mmdd,i,ii,n,k,ix,itail
+integer :: instance,iostatus
+logical :: jf(12)
+data sw/25*1./
 
 !
 ! Initialize standard switches:
 !
-  iscale=1
-  xuvfac=3.
-  kchem=4
-  jlocal=0
-  itail=0
-  fmono=0.
-  emono=0.
+iscale=1
+xuvfac=3.
+kchem=4
+jlocal=0
+itail=0
+fmono=0.
+emono=0.
 !
 ! Set data directories:
 !
-  data_dir    = 'data/'
-  iri90_dir   = 'data/iri90/'
+data_dir    = 'data/'
+iri90_dir   = 'data/iri90/'
 !
 ! Set number of altitude levels:
 !
-  jmax = 102
+jmax = 102
 !
 ! Allocate local arrays:
 !
-  allocate(z(jmax))
-  allocate(zun(jmax))
-  allocate(zvn(jmax))
-  allocate(pedcond(jmax))
-  allocate(hallcond(jmax))
-  allocate(outf(11,jmax))
+allocate(z(jmax))
+allocate(zun(jmax))
+allocate(zvn(jmax))
+allocate(pedcond(jmax))
+allocate(hallcond(jmax))
+allocate(outf(11,jmax))
 !
 ! Call CGLOW_INIT (module CGLOW) to set array dimensions and allocate use-associated variables:
 ! (This was formerly done using common blocks, including common block /cglow/.)
 !
-  call cglow_init
+call cglow_init
 !
 ! Call EGRID to set up electron energy grid:
 !
-  call egrid (ener, del, nbins)
-!
-! Loop to call GLOW for designated inputs until end-of-file or any character on standard input:
-!
-  do instance=1,10000
+call egrid (ener, del, nbins)
+
 !
 ! Get input values:
-!
-    write(6,"('Enter date, UT, lat, lon, F107a, F107, F107p, Ap, Ef, Ec')")
-    read(5,*,iostat=iostatus) idate,ut,glat,glong,f107a,f107,f107p,ap,ef,ec
-    if (iostatus /= 0) stop
-!
+if (command_argument_count() < 1) error stop 'please tell me what .nml input to use'
+call get_command_argument(1, config_filename)
+open(newunit=u, file=config_filename, status='old', action='read')
+read(u,nml=input)
+close(u)
 ! Calculate local solar time:
 !
-    stl = ut/3600. + glong/15.
-    if (stl < 0.) stl = stl + 24.
-    if (stl >= 24.) stl = stl - 24.
+stl = ut/3600. + glong/15.
+if (stl < 0.) stl = stl + 24.
+if (stl >= 24.) stl = stl - 24.
 !
 ! Call MZGRID to use MSIS/NOEM/IRI inputs on default altitude grid:
 !
-    call mzgrid (jmax,nex,idate,ut,glat,glong,stl,f107a,f107,f107p,ap,iri90_dir, &
-                 z,zo,zo2,zn2,zns,znd,zno,ztn,zun,zvn,ze,zti,zte,zxden)
+call mzgrid (jmax,nex,idate,ut,glat,glong,stl,f107a,f107,f107p,ap,iri90_dir, &
+             z,zo,zo2,zn2,zns,znd,zno,ztn,zun,zvn,ze,zti,zte,zxden)
 !
 ! Call MAXT to put auroral electron flux specified by namelist input into phitop array:
 !
-    phitop(:) = 0.
-    if (ef>.001 .and. ec>1.) call maxt (ef,ec,ener,del,nbins,itail,fmono,emono,phitop)
+phitop(:) = 0.
+if (ef>.001 .and. ec>1.) call maxt (ef,ec,ener,del,nbins,itail,fmono,emono,phitop)
 !
 ! Fill altitude array, converting to cm:
 !
-    zz(:) = z(:) * 1.e5     ! km to cm at all jmax levels
+zz(:) = z(:) * 1.e5     ! km to cm at all jmax levels
 !
 ! Call GLOW to calculate ionized and excited species, airglow emission rates,
 ! and vertical column brightnesses:
 !
-    call glow
+call glow
 !
 ! Call CONDUCT to calculate Pederson and Hall conductivities:
 !
-    do j=1,jmax
-      call conduct (glat, glong, z(j), zo(j), zo2(j), zn2(j), &
-                    zxden(3,j), zxden(6,j), zxden(7,j), ztn(j), zti(j), zte(j), &
-                    pedcond(j), hallcond(j))
-    enddo
+do j=1,jmax
+  call conduct (glat, glong, z(j), zo(j), zo2(j), zn2(j), &
+                zxden(3,j), zxden(6,j), zxden(7,j), ztn(j), zti(j), zte(j), &
+                pedcond(j), hallcond(j))
+enddo
 !
 ! Output section:
 !
-    write(6,"(1x,i7,9f8.1)") idate,ut,glat,glong,f107a,f107,f107p,ap,ef,ec
-    write(6,"('   Z     Tn       O        N2        NO      Ne(in)    Ne(out)  Ionrate      O+       O2+      NO+       N(2D)    Pederson   Hall')")
-    write(6,"(1x,0p,f5.1,f6.0,1p,12e10.2)") (z(j),ztn(j),zo(j),zn2(j),zno(j),ze(j), &
-      ecalc(j),tir(j),zxden(3,j),zxden(6,j),zxden(7,j),zxden(10,j),pedcond(j),hallcond(j),j=1,jmax)
-    write(6,"('   Z      3371    4278    5200    5577    6300    7320   10400    3644    7774    8446    3726    LBH     1356    1493    1304')")
-    write(6,"(1x,f5.1,15f8.2)")(z(j),(zeta(ii,j),ii=1,15),j=1,jmax)
-
-  enddo
-
-stop
+write(stdout,"(1x,i7,9f8.1)") idate,ut,glat,glong,f107a,f107,f107p,ap,ef,ec
+write(stdout,"(A)") '   Z     Tn       O        N2        NO      Ne(in)    Ne(out)  Ionrate' &
+                  //'O+       O2+      NO+       N(2D)    Pederson   Hall'
+write(stdout,"(1x,0p,f5.1,f6.0,1p,12e10.2)") (z(j),ztn(j),zo(j),zn2(j),zno(j),ze(j), &
+  ecalc(j),tir(j),zxden(3,j),zxden(6,j),zxden(7,j),zxden(10,j),pedcond(j),hallcond(j),j=1,jmax)
+write(stdout,"(A)") '   Z      3371    4278    5200    5577    6300    7320   10400    3644    7774' &
+                  //'    8446    3726    LBH     1356    1493    1304'
+write(stdout,"(1x,f5.1,15f8.2)")(z(j),(zeta(ii,j),ii=1,15),j=1,jmax)
 
 end program glowbasic
