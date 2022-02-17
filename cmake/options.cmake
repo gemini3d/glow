1833c5591e8fec995eb6e8d0c6606236adf7174b
Michael Hirsch <scivision@users.noreply.github.com>
2022-02-17 09:30:46 -0500

    msvc/windows oneapi shared lib parameter

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -3,6 +3,9 @@ set(CMAKE_INSTALL_NAME_DIR ${CMAKE_INSTALL_PREFIX}/lib)
 set(CMAKE_INSTALL_RPATH ${CMAKE_INSTALL_PREFIX}/lib)
 set(CMAKE_INSTALL_RPATH_USE_LINK_PATH true)
 
+# Necessary for shared library with Visual Studio / Windows oneAPI
+set(CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS true)
+
 if(NOT EXISTS ${PROJECT_BINARY_DIR}/.gitignore)
   file(WRITE ${PROJECT_BINARY_DIR}/.gitignore "*")
 endif()
